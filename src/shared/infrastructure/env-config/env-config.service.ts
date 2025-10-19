dfdcfeb3bcfc5bbf9e0e98d2715af9e68d1514d5
joseerisvaldopg <joseerisvaldopg@gmail.com>
2025-10-19 14:57:10 -0300

    feat: implement envConfig

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-congi-interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private readonly configService: ConfigService) {}
+
+  getAppPort(): number {
+    return this.configService.get<number>('APP_PORT', 3000);
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV', 'development');
+  }
+}
